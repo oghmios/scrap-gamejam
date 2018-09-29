3b6be0d4ac5de6ccb42ba0a23bcbca58931fd8e3
Oghmios <pdepablito@gmail.com>
2018-09-29 02:43:28 +0200

    Added blocks

---
+++ 20180929_SCRAP_GAMEJAM/Assets/Resources/Scripts/Blocks/BlocksLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class BlocksLine : MonoBehaviour {
+
+    public GameObject[] blockTypes;
+    private GameObject[] lineOfBlocks;
+    public Vector3 position;
+    public Quaternion rotation;
+    private System.Random randomBlockNumber;
+    
+	// Use this for initialization
+	void Start () {
+        randomBlockNumber = new System.Random();
+        lineOfBlocks = new GameObject[10];
+        for(int i = 0; i < 10; i++)
+        {
+            int randomType = randomBlockNumber.Next(0, 4);
+            lineOfBlocks[i] = (GameObject)Instantiate(blockTypes[randomType], position, rotation);
+            position.x += 3;
+
+        }
+	}
+
+}
