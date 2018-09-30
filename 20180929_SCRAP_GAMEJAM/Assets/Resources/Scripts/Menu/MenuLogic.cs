eb4bcbf573a5c4de8057aec922b9ca6d8c7ca22a
Oghmios <pdepablito@gmail.com>
2018-09-30 12:15:24 +0200

    Añadido background al menu Añadida fuente Fix de efecto zoom en letras

--- 20180929_SCRAP_GAMEJAM/Assets/Resources/Scripts/Menu/MenuLogic.cs
+++ 20180929_SCRAP_GAMEJAM/Assets/Resources/Scripts/Menu/MenuLogic.cs
@@ -11,7 +11,7 @@ public class MenuLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButton("Jump"))
-			Application.LoadLevel ("Game");
+			Application.LoadLevel ("Level 1");
 
 		if (Input.GetKey(KeyCode.Escape))
 			Application.Quit();
